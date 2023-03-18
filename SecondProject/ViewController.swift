1ae40cd5c26a5aed31ec55ec3712c28eed070128
Ekaterina Bogdanova <katarina.bog@icloud.com>
2023-03-18 20:38:29 +0700

    Initial Commit

---
+++ SecondProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondProject
+//
+//  Created by Ekaterina Bogdanova on 18/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
