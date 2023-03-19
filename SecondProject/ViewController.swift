9a51b589e6898c35b8586fa2b0419d52559aafdc
Ekaterina Bogdanova <katarina.bog@icloud.com>
2023-03-20 03:10:44 +0700

    Fixed brackets

--- SecondProject/ViewController.swift
+++ SecondProject/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         makeRoundViewElement()
     }
+    
     private func makeRoundViewElement() {
         redLight.layer.cornerRadius = redLight.bounds.height / 2
         yellowLight.layer.cornerRadius = yellowLight.bounds.height / 2
@@ -29,29 +30,28 @@ class ViewController: UIViewController {
     
 
         
-        @IBAction func startButtonDidTapped(startButton: UIButton) {
-            if startButton.currentTitle == "START" {
-                startButton.setTitle("NEXT", for: .normal)
-                redLight.alpha = 1
-            } else {
-                switch currentLight {
-                case 0:
-                    redLight.alpha = 0.3
-                    yellowLight.alpha = 1.0
-                    currentLight += 1
-                case 1:
-                    yellowLight.alpha = 0.3
-                    greenLight.alpha = 1.0
-                    currentLight += 1
-                case 2:
-                    greenLight.alpha = 0.3
-                    redLight.alpha = 1.0
-                    currentLight = 0
-                default:
-                    break
-                }
-                
+    @IBAction func startButtonDidTapped(startButton: UIButton) {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+            redLight.alpha = 1
+        } else {
+            switch currentLight {
+            case 0:
+                redLight.alpha = 0.3
+                yellowLight.alpha = 1.0
+                currentLight += 1
+            case 1:
+                yellowLight.alpha = 0.3
+                greenLight.alpha = 1.0
+                currentLight += 1
+            case 2:
+                greenLight.alpha = 0.3
+                redLight.alpha = 1.0
+                currentLight = 0
+            default:
+                break
             }
         }
     }
+}
 
